5bac54bd177e5da287740a98b8cf268042c5b063
Roman Peters <github@romanpeters.nl>
2021-02-25 23:24:47 +0100

    Add writer page

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from sqlalchemy import or_
 import base64
 import markdown
@@ -10,6 +10,10 @@ from app import app
 def index():
     return render_template("index.html")
 
+
+def encode_url(text):
+    url_path = base64.urlsafe_b64encode(bytes(text, 'utf-8')).decode('utf-8')
+    return url_path
 
 def decode_url(url_path):
     text = base64.urlsafe_b64decode(f'{url_path}===').decode('utf-8')
@@ -35,6 +39,16 @@ def read_text(url_path):
             print(e)
             return render_template("404.html")
     return render_template('reader.html', title=title, html=html)
+
+@app.route("/writer", methods=['GET', 'POST'])
+def writer():
+    if request.method == 'POST':
+        text = request.form['text']
+        if text:
+            url_path = encode_url(text)
+            print(url_path)
+            return render_template('writer.html', url=f"{request.host_url}{url_path}")
+    return render_template("writer.html")
 
 
 
